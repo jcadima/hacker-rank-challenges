5f02c98c39b8aed24c70b64f0a3c7812a64e67b8
Juan Cadima <juanjcadima@gmail.com>
2016-09-04 16:27:30 -0400

    init commit

---
+++ algorithms/implementation/kangaroo.php
@@ -0,0 +1,19 @@
+<?php
+/*
+There are two kangaroos on an x-axis ready to jump in the positive direction (i.e, toward positive infinity). The first kangaroo starts at location x_1 and moves at a rate of v_1 meters per jump. The second kangaroo starts at location x_2 and moves at a rate of v_2 meters per jump. Given the starting locations and movement rates for each kangaroo, can you determine if they'll ever land at the same location at the same time?
+
+> Print YES if they can land on the same location at the same time; otherwise, print NO
+*/
+
+$handle = fopen ("php://stdin","r");
+fscanf($handle,"%d %d %d %d",$x1,$v1,$x2,$v2);
+$kang1 = $x1;  $kang2 = $x2;  $meet = false;
+for ($i = 0 ; $i < 50; $i++ ) {
+	$kang1 +=  $v1 ;
+	$kang2 +=  $v2 ;
+	if( $kang1 == $kang2) {
+		$meet = true;
+	}
+}
+echo ($meet) ? "YES" : "NO" ;
+
