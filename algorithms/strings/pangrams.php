7423409d65b55dc1913607901c6f3d408ab665f2
Juan Cadima <juanjcadima@gmail.com>
2016-09-15 03:52:26 -0400

    updated pangrams

--- algorithms/strings/pangrams.php
+++ algorithms/strings/pangrams.php
@@ -1,10 +1,12 @@
 <?php
 /*
-Roy wanted to increase his typing speed for programming contests. So, his friend advised him to type the sentence "The quick brown fox jumps over the lazy dog" repeatedly, because it is a pangram. (Pangrams are sentences constructed by using every letter of the alphabet at least once.)
+Roy wanted to increase his typing speed for programming contests. So, his friend advised him to 
+type the sentence "The quick brown fox jumps over the lazy dog" repeatedly, because it is a pangram. (Pangrams are sentences constructed by using every letter of the alphabet at least once.)
 
-After typing the sentence several times, Roy became bored with it. So he started to look for other pangrams.
+After typing the sentence several times, Roy became bored with it. 
+So he started to look for other pangrams.
 
-Given a sentence s, tell Roy if it is a pangram or not.
+Given a sentence (string) s, tell Roy if it is a pangram or not.
 */
 
 
