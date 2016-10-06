6574600db4d93f2ac994a65bd13a41d20fd01f87
Juan Cadima <juanjcadima@gmail.com>
2016-10-06 19:36:02 -0400

    dictionaries and maps

--- dictionaries_and_maps.php
+++ dictionaries_and_maps.php
@@ -4,7 +4,22 @@ Given "n" names and phone numbers, assemble a phone book that maps friends' name
 You will then be given an unknown number of names to query your phone book for. For each "name" queried, 
 print the associated entry from your phone book on a new line in the form name=phoneNumber; 
 if an entry for "name" is not found, print Not found instead.
+
+Sample Input:
+3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+edward
+harry
+
+Sample Output:
+sam=99912222
+Not found
+harry=12299933
 */
+
 
 $phonebook = array();
 $limit = intval(fgets(STDIN));
