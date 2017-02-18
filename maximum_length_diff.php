ddb4c5585bfbd92f78c1a9aa4889458daf2308aa
Juan Cadima <juanjcadima@gmail.com>
2017-02-18 12:32:24 -0500

    Maximum Length Difference

---
+++ maximum_length_diff.php
@@ -0,0 +1,44 @@
+<?php
+/*
+You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(s1, s2) --> 13
+
+*/
+
+
+function mxdiflg($a1, $a2) {
+$max = 0;
+if( empty($a1) || empty($a2) ) {
+  return -1;
+}
+
+// Compare each value from array1 with array2
+  for ($i = 0; $i < count($a1)   ; $i++ ) {
+  	for($j = 0 ; $j < count($a2)  ; $j++  ) {
+  		$current =  abs(strlen($a1[$i]) - strlen($a2[$j])  ) ;
+  
+  		if( $current > $max ) {	
+  			$max = abs(strlen($a1[$i]) - strlen($a2[$j])  ); 
+  			
+  		}		
+  	}
+  }
+  return $max;
+}
+
+$s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"];
+$s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"];
+
+echo  mxdiflg($s1, $s2); // 13
+
+
+
+
+
+
+
