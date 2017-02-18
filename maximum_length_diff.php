d7704b84740507400ad3c86725c1dba830337c1e
Juan Cadima <juanjcadima@gmail.com>
2017-02-18 12:36:06 -0500

    Maximum length difference

--- maximum_length_diff.php
+++ maximum_length_diff.php
@@ -4,33 +4,30 @@ You are given two arrays a1 and a2 of strings. Each string is composed with lett
 
 Find max(abs(length(x) − length(y)))
 
-s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
-s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
-mxdiflg(s1, s2) --> 13
-
 */
 
 
 function mxdiflg($a1, $a2) {
-$max = 0;
-if( empty($a1) || empty($a2) ) {
-  return -1;
+	$max = 0;
+	if( empty($a1) || empty($a2) ) {
+	  return -1;
+	}
+
+	// Compare each value from array1 with array2
+	  for ($i = 0; $i < count($a1)   ; $i++ ) {
+	  	for($j = 0 ; $j < count($a2)  ; $j++  ) {
+	  		$current =  abs(strlen($a1[$i]) - strlen($a2[$j])  ) ;
+	  
+	  		if( $current > $max ) {	
+	  			$max = abs(strlen($a1[$i]) - strlen($a2[$j])  ); 
+	  			
+	  		}		
+	  	}
+	  }
+	  return $max;
 }
 
-// Compare each value from array1 with array2
-  for ($i = 0; $i < count($a1)   ; $i++ ) {
-  	for($j = 0 ; $j < count($a2)  ; $j++  ) {
-  		$current =  abs(strlen($a1[$i]) - strlen($a2[$j])  ) ;
-  
-  		if( $current > $max ) {	
-  			$max = abs(strlen($a1[$i]) - strlen($a2[$j])  ); 
-  			
-  		}		
-  	}
-  }
-  return $max;
-}
-
+// EXAMPLE
 $s1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"];
 $s2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"];
 
